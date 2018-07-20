813eccdd10bae14eacbf6fade8de2d3f9f82d95e
senavarrete <senavarrete@udec.cl>
2018-07-20 19:29:59 -0400

    page navigator agregado

--- Busqueda.php
+++ Busqueda.php
@@ -176,8 +176,23 @@ div {
 
       </div>
       <!-- /.row -->
+      <div class="text-center">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
 
+    <li class="page-item disabled">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</div>
     </div>
+
+    <!-- Number Navigator-->
+
+
     <footer class="container">
       <p>&copy; Company 2017-2018</p>
     </footer>
