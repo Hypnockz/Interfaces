3aafbfe61f264c0cf8774b9d689f45e4f17046e9
Esteban <estebanibaca@udec.cl>
2018-07-20 17:12:57 -0400

    agregada barra nav

---
+++ includes/barranavegacion.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header" style="width:15%">
+      <a class="navbar-brand" href="#">DealsWatcher</a>
+    </div>
+    <ul class="nav navbar-nav" style="width:15%">
+
+      <li class="dropdown" style="width:100%;text-align:center">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="width:100%">Categorías
+              <span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width:100%">
+          <li><a href="#">Lácteos</a></li>
+          <li><a href="#">Frutas</a></li>
+          <li><a href="#">Carnes</a></li>
+      </li>
+
+      </ul>
+
+
+    </ul>
+
+    <form class="navbar-form navbar-left" action="#" style="width:40%">
+      <div class="form-group" style="width:75%">
+        <input type="text" class="form-control" placeholder="¿Qué estás buscando?" style="width:100%">
+      </div>
+      <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+    </form>
+
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-list-alt"></span> Mis Listas</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Mi Cuenta</a></li>
+    </ul>
+  </div>
+</nav>
