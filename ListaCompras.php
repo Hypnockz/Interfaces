b3989d32f40a0aa69de0a4674bc17eebea431daa
zhikul <39314853+zhikul@users.noreply.github.com>
2018-07-20 21:51:13 -0400

    lista de compra

---
+++ ListaCompras.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="en">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/css/Carousel.css">
+
+
+
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+
+    <title>Welcome to DealsWatchers!</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+  <?php require 'includes/barranavegacion.php' ?>
+
+      <main role="main">
+        <div class="col-md-1"></div>
+        <div class="container col-md-10">
+  <div class="row">
+    <div class="col-xs-8 col-md-12 col-lg-12">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          <div class="panel-title">
+            <div class="row">
+              <div class="col-xs-6">
+                <h5><span class="glyphicon glyphicon-list-alt"></span> Lista de Compras <span>></span> lista1 </h5>
+              </div>
+              <div class="col-xs-6">
+                <button type="button" class="btn btn-primary btn-sm btn-block">
+                  <span class="glyphicon glyphicon-arrow-left"></span> Siga Cotizando
+                </button>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-6">
+                
+              </div>
+              <div class="col-xs-6">
+                <div class="col-xs-6 text-right">¿Te ayudamos?</div>
+                <div class="col-xs-6">
+                 <div class="form-group">
+                    <select class="form-control" id="sel1">
+                          <option>Personalizado</option>
+                          <option>Supermercado más barato</option>
+                          <option>Menor precio total</option>
+                          
+                    </select>
+                  </div> 
+                  </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-2"></div>
+              <div class="col-xs-4 ">
+                Nombre
+              </div>
+              <div class="col-xs-2 ">
+                cantidad
+              </div>
+              <div class="col-xs-2 ">
+                Tienda
+              </div>
+              <div class="col-xs-2 text-center">
+                Precio
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-xs-2"><img class="img-responsive" src="http://placehold.it/100x70">
+            </div>
+            <div class="col-xs-4">
+              <h4 class="product-name"><strong>Product name</strong></h4><h4><small>Product description</small></h4>
+            </div>
+            <div class="col-xs-2">
+              <div class="col-xs-6">
+                <input type="text" class="form-control input-sm" value="1">
+              </div>
+              </div>
+            <div class="col-xs-2 form-group">
+              <select class="form-control" id="sel1">
+                          <option>Lider</option>
+                          <option>Jumbo</option>
+                          <option>Santa Isabel</option>
+                          <option>Tottus</option>
+                          <option>Unimarc</option>
+                          
+                    </select>
+            </div>
+            <div class="col-xs-2">
+              <div class="col-xs-8 text-right">
+                <h6><strong>18.500 </strong></h6>
+              </div>
+              
+              <div class="col-xs-4">
+                <button type="button" class="btn btn-danger">
+                  <span class="glyphicon glyphicon-remove"> </span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <hr>
+          
+          <div class="row">
+            <div class="text-center">
+              <div class="col-xs-9">
+                <h6 class="text-right">¿Agrego más artículos?</h6>
+              </div>
+              <div class="col-xs-3">
+                <button type="button" class="btn btn-default btn-sm btn-block">
+                  <span class="glyphicon glyphicon-repeat"></span>
+                  Actualize la lista
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="panel-footer">
+          <div class="row text-center">
+            <div class="col-xs-9"></div>
+            <div class="col-xs-3">
+              <h4 class="text-right">Total <strong>$50.00</strong></h4>
+            </div>
+          </div>
+          <div class="row">  
+            <div class="col-xs-6">
+              <button type="button" class="btn btn-info">Exportar</button>
+              <button type="button" class="btn btn-info">Generar Captura de Pantalla</button>
+              <button type="button" class="btn btn-danger">Eliminar</button>
+            </div>
+            <div class="col-xs-3">
+              
+            </div>
+            <div class="col-xs-3">
+              <button type="button" class="btn btn-success btn-block">
+                Guardar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+         </main>
+
+    <footer class="container">
+      <p>&copy; Company 2017-2018</p>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+    <script>window.jQuery || document.write('<script src="assets/js/jquery-slim.min.js"><\/script>')</script>
+    <script src="assets/js/popper.min.js"></script>
+  </body>
+
+
+<style>
+
+body{
+padding-top: 0px;
+}
+
+</style>
+</html>
